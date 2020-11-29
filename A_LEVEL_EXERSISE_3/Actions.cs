a0c5d212d493a5bd3d492959556e089f7f324d17
gromolga <gromolga.work@gmail.com>
2020-11-29 11:57:40 +0200

    created file system; added singleton construction to logger

---
+++ A_LEVEL_EXERSISE_3/Actions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A_LEVEL_EXERSISE_3
+{
+    class Actions
+    {
+    }
+}
---
+++ A_LEVEL_EXERSISE_3/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace A_LEVEL_EXERSISE_3
+{
+    class Logger
+    {
+        private static Logger instance;
+
+        private Logger()
+        {
+
+        }
+        public static Logger getInstance()
+        {
+            if (instance == null)
+                instance = new Logger();
+            return instance;
+        }
+    }
+}
